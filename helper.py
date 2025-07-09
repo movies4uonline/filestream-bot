63fd08435dbec222a5162c149b0dd8f25406b981
movies4uonline <moviesu537@gmail.com>
2025-07-10 00:32:13 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,6 @@
+def readable_size(size_bytes):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size_bytes < 1024:
+            return f"{size_bytes:.2f} {unit}"
+        size_bytes /= 1024
+    return f"{size_bytes:.2f} PB"
